4c347bd3765697bf5d9e730159fc3676793cc221
onnorom <obijinnorom@gmail.com>
2018-04-28 22:36:13 -0500

    init phantomjs program

---
+++ src/test/java/obiji/WebDriverBase.java
@@ -0,0 +1,27 @@
+package obiji;
+
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.WebDriver;
+
+public class WebDriverBase {
+
+  static protected WebDriver driver;
+
+  public static void setup() {
+    DesiredCapabilities caps = new DesiredCapabilities();
+    caps.setJavascriptEnabled(true); // not really needed: JS enabled by default
+    caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "src/test/resources/drivers/phantomjs");
+
+    driver = new PhantomJSDriver(caps);
+  }
+
+  public static void main(String[] args) {
+    WebDriverBase.setup();
+    driver.get("https://www.testingexcellence.com");
+    String pageTitle = driver.getTitle();
+    System.out.println("Page opened: {}"+pageTitle);
+    driver.quit();
+  }
+}
